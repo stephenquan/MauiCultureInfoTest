2afd2e51ed5805ae229c7bce3297053923067eb5
Stephen Quan <squan@esri.com>
2025-02-26 11:20:38 +1100

    Broad CultureInfo test

---
+++ MauiCultureInfoTest/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+
+namespace MauiCultureInfoTest;
+
+/// <summary>
+/// A test page for troubleshooting iOS specific language culture issues.
+/// </summary>
+public partial class MainPage : ContentPage
+{
+	/// <summary>
+	/// Exposes the installed UI culture for the app.
+	/// </summary>
+	public CultureInfo InstalledUICulture => CultureInfo.InstalledUICulture;
+
+	/// <summary>
+	/// Exposes the current UI culture for the app.
+	/// </summary>
+	public CultureInfo CurrentUICulture => CultureInfo.CurrentUICulture;
+
+	/// <summary>
+	/// Exposes the current culture for the app.
+	/// </summary>
+	public CultureInfo CurrentCulture => CultureInfo.CurrentCulture;
+
+	/// <summary>
+	/// Returns the date time when the culture information was last checked.
+	/// </summary>
+	public DateTime LastChecked => DateTime.Now;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MainPage"/> class.
+	/// </summary>
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	/// <summary>
+	/// Handles the refresh button click event.
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	void OnRefresh(object sender, EventArgs e)
+	{
+		CultureInfo.InstalledUICulture.ClearCachedData();
+		CultureInfo.CurrentUICulture.ClearCachedData();
+		CultureInfo.CurrentCulture.ClearCachedData();
+		OnPropertyChanged(nameof(InstalledUICulture));
+		OnPropertyChanged(nameof(CurrentUICulture));
+		OnPropertyChanged(nameof(CurrentCulture));
+		OnPropertyChanged(nameof(LastChecked));
+	}
+}
---
+++ MauiCultureInfoTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiCultureInfoTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
